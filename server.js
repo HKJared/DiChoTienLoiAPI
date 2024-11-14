e09c071ac2512675dbd1e740947389ae6612d2c6
KHJared <vuq147@gmail.com>
2024-11-14 23:02:50 +0700

    lock passport

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const session = require('express-session');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const passport = require('./src/utils/auth');
+// const passport = require('./src/utils/auth');
 
 // auth router
-const authRouter = require('./src/routes/auth/authRouter');
+// const authRouter = require('./src/routes/auth/authRouter');
 
 //api router
 const adminApiRouter = require('./src/routes/api/adminRouter');
@@ -17,21 +17,21 @@ const app = express();
 
 const port = process.env.PORT || 8888;
 
-// Middleware Passport
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// // Middleware Passport
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
 
-app.use('/auth/', authRouter)
+// app.use('/auth/', authRouter)
 
 app.use('/api/admin/', adminApiRouter);
 app.use('/api/user/', userApiRouter);
